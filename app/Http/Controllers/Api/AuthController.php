1f027f747bde6b12999e3649ba6df12a06cd0af9
Artem Kuptsov <kuptsov210@gmail.com>
2022-07-10 16:30:49 +0300

    Fix mistakes

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,27 +5,25 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\Auth\LoginRequest;
 use App\Http\Requests\Api\Auth\RegisterFormRequest;
-use App\Models\User;
 use App\Services\UserService;
 use Illuminate\Http\Request;
-use Illuminate\Support\Carbon;
-use Illuminate\Support\Facades\Auth;
-use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Http\Response;
+
 
 class AuthController extends Controller
 {
-    public function register(RegisterFormRequest $request, UserService $userService)
+    public function register(RegisterFormRequest $request, UserService $userService): Response
     {
         return response($userService->register($request));
     }
 
-    public function login(LoginRequest $request, UserService $userService)
+    public function login(LoginRequest $request, UserService $userService): Response
     {
         $result = $userService->login($request);
         return response($result, $result['code']);
     }
 
-    public function logout(Request $request, UserService $userService)
+    public function logout(Request $request, UserService $userService): Response
     {
         return response($userService->logout($request));
     }
